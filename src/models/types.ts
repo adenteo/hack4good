cc6fa1e877350b8c816f5be955c7fe292504c1d5
ethan-cyj <e0773997@u.nus.edu>
2024-02-01 01:45:11 +0800

    added activity, attendance, certificates, volunteer models

--- src/models/types.ts
+++ src/models/types.ts
@@ -2,11 +2,54 @@ export enum UserStatus {
     Active = "Active",
     Banned = "Banned",
     Deleted = "Deleted",
-  }
+}
   
-  export enum RoleName {
+export enum RoleName {
     Admin = "Admin",
-    ActivityLead = "ActivityLead",
     Volunteer = "Volunteer",
-  }
+}
   
+  // src/models/types.ts
+export enum VolunteerStatus {
+    Active = 'Active',
+    Deleted = 'Deleted',
+    Banned = 'Banned',
+}
+
+export enum Gender {
+Male = 'Male',
+Female = 'Female',
+Other = 'Other',
+}
+
+export enum CitizenshipType {
+Singaporean = 'Singaporean',
+SingaporePermanentResident = 'Singapore Permanent Resident',
+LongTermVisitPass = 'Long Term Visit Pass',
+EmploymentPass = 'Employment Pass / S Pass',
+}
+
+export enum EmploymentStatus {
+Employed = 'Employed / Self- Employed',
+Unemployed = 'Unemployed',
+Student = 'Student',
+Retired = 'Retired',
+Other = 'Other', // Use this with a free text field
+}
+
+export enum DrivingLicence {
+Yes = 'Yes',
+No = 'No',
+}
+
+export enum AttendanceStatus {
+    Present = 'Present',
+    Absent = 'Absent',
+    Unconfirmed = 'Unconfirmed',
+}
+
+export enum CertificateType {
+Completion = 'Completion',
+Milestone = 'Milestone',
+Special = 'Special',
+}
