5e36c0bcca9812fadddc00ac6a50d4b9bb2976f1
ethan-cyj <e0773997@u.nus.edu>
2024-02-04 15:16:18 +0800

    fixed syntax error

--- src/lib/actions/add-activities.ts
+++ src/lib/actions/add-activities.ts
@@ -2,9 +2,12 @@
 // Import the Activity model
 import { connectToDB } from '../mongoose';
 import Activity from '@/models/Activity';
-
+interface ActivityData {
+    name: string;
+  }
+  
 // Function to create an Activity
-export async function addActivity(activityData: object) {
+export async function addActivity(activityData: ActivityData) {
   // Create a new Activity document
   await connectToDB();
   const existingActivity = await Activity.findOne({ name: activityData.name});
@@ -20,20 +23,20 @@ export async function addActivity(activityData: object) {
   }
 }
 
-//const newActivityData = { //example data
-//  name: 'Community Cooking Event',
-//  address: '123 Main St, Cookville',
-//  description: 'Join us as we prepare warm meals for the elderly in our community.',
-//  day: 6, 
-//  startTime: new Date('2024-04-01T09:00:00'),
-//  endTime: new Date('2024-04-01T15:00:00'),
-//  volunteerCountNeeded: 10,
-//  pointOfContact: 'ObjectIdOfTheUser', // Replace with actual ObjectId
-//  signUpLimit: 20,
-//  imgUrl: 'urlToTheImageForTheEvent',
-//  signUpDeadline: new Date('2024-03-25T23:59:59'),
-//  attendees: [],
-//};
-//
-//addActivity(newActivityData)
-//  .catch(error => console.error('Error creating activity:', error));
+const newActivityData = { //example data
+  name: 'Community Cooking Event',
+  address: '123 Main St, Cookville',
+  description: 'Join us as we prepare warm meals for the elderly in our community.',
+  day: 6, 
+  startTime: new Date('2024-04-01T09:00:00'),
+  endTime: new Date('2024-04-01T15:00:00'),
+  volunteerCountNeeded: 10,
+  pointOfContact: 'ObjectIdOfTheUser', // Replace with actual ObjectId
+  signUpLimit: 20,
+  imgUrl: 'urlToTheImageForTheEvent',
+  signUpDeadline: new Date('2024-03-25T23:59:59'),
+  attendees: [],
+};
+
+addActivity(newActivityData)
+  .catch(error => console.error('Error creating activity:', error));
