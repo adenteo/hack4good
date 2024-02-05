fe9d7b5f87d5e17989b3c830b1f2f9619e5f1949
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-02-05 20:50:57 +0800

    Fix build errors

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -1,20 +1,20 @@
 // src/models/Activity.ts
-import {Schema, model, models } from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 import { AttendanceStatus, volunteerTheme } from './types';
 
 const attendeeSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   role: { type: String, required: true },
-  signUpFormDetails: { type: Schema.Types.Mixed }, // Flexible field 
-  attendanceStatus: { 
-    type: String, 
-    enum: Object.values(AttendanceStatus), 
-    default: AttendanceStatus.Unconfirmed 
+  signUpFormDetails: { type: Schema.Types.Mixed }, // Flexible field
+  attendanceStatus: {
+    type: String,
+    enum: Object.values(AttendanceStatus),
+    default: AttendanceStatus.Unconfirmed,
   },
 });
 
 const activitySchema = new Schema({
-  title: { type: String, required: true }, 
+  title: { type: String, required: true },
   address: { type: String, required: true },
   description: { type: String, required: true },
   additionalDetails: { type: String, required: false },
@@ -23,17 +23,15 @@ const activitySchema = new Schema({
   numPeopleJoined: { type: Number, required: true },
   volunteerCountNeeded: { type: Number, required: false },
   numHours: { type: Number, required: true },
-  pointOfContact: { type: Schema.Types.ObjectId, ref: 'User'},
+  pointOfContact: { type: Schema.Types.ObjectId, ref: 'User' },
   signUpLimit: { type: Number, required: false },
   image: { type: String, required: true },
   signUpDeadline: { type: Date, required: true },
-  tags: {type: Array, required:true},
-  activitySignupForm: { type: Schema.Types.ObjectId, ref: 'CustomForm'},
+  activitySignupForm: { type: Schema.Types.ObjectId, ref: 'CustomForm' },
   attendees: [attendeeSchema],
-  tags: { type: [String], enum: Object.values(volunteerTheme)},
+  tags: { type: [String], enum: Object.values(volunteerTheme) },
 });
 
-
-const Activity = models.Activity ||  model('Activity', activitySchema);
+const Activity = models.Activity || model('Activity', activitySchema);
 
 export default Activity;
