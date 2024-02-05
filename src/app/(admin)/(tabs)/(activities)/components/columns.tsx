1217ab99f82e827a067a3dbdddfb2e91c8b6809b
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-02-05 23:35:15 +0800

    Add color to activity statuses

--- src/app/(admin)/(tabs)/(activities)/components/columns.tsx
+++ src/app/(admin)/(tabs)/(activities)/components/columns.tsx
@@ -101,10 +101,23 @@ export const columns: ColumnDef<any>[] = [
 
       return (
         <div className="flex w-[100px] items-center">
-          {status.icon && (
-            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          {status.label == 'Completed' ? (
+            <span className="bg-green-500 p-2 rounded-full text-xs text-white">
+              {status.label}
+            </span>
+          ) : status.label == 'Upcoming' ? (
+            <span className="bg-blue-500 p-2 rounded-full text-xs text-white">
+              {status.label}
+            </span>
+          ) : status.label == 'Ongoing' ? (
+            <span className="bg-orange-500 p-2 rounded-full text-xs text-white">
+              {status.label}
+            </span>
+          ) : (
+            <span className="bg-red-500 p-2 rounded-full text-xs text-white">
+              {status.label}
+            </span>
           )}
-          <span>{status.label}</span>
         </div>
       );
     },
@@ -113,7 +126,61 @@ export const columns: ColumnDef<any>[] = [
     },
   },
   {
+    accessorKey: 'attendees',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Volunteer Count" />
+    ),
+    cell: ({ row }: any) => {
+      const attendees = row.getValue('attendees');
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {attendees.length}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'signUpLimit',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Max Cap." />
+    ),
+    cell: ({ row }: any) => {
+      const limit = row.getValue('signUpLimit');
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">{limit}</span>
+        </div>
+      );
+    },
+  },
+  {
     id: 'actions',
     cell: ({ row }) => <DataTableRowActions row={row} />,
   },
 ];
+
+const StatusComponent = (status: any) => {
+  let backgroundColor;
+
+  switch (status) {
+    case 'Completed':
+      backgroundColor = 'green';
+      break;
+    case 'Upcoming':
+      backgroundColor = 'blue';
+      break;
+    case 'Ongoing':
+      backgroundColor = 'yellow';
+      break;
+    case 'Cancelled':
+      backgroundColor = 'red';
+      break;
+    default:
+      backgroundColor = 'gray';
+      break;
+  }
+
+  return <span style={{ backgroundColor }}>{status.label}</span>;
+};
