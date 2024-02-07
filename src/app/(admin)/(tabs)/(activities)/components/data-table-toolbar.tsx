7f887e9bb9cd906f239406ca265ebb51a4ba044d
adenteo <adenteo@gmail.com>
2024-02-08 01:33:45 +0800

    Add loading & fix placeholders

--- src/app/(admin)/(tabs)/(activities)/components/data-table-toolbar.tsx
+++ src/app/(admin)/(tabs)/(activities)/components/data-table-toolbar.tsx
@@ -25,7 +25,7 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter tasks..."
+          placeholder="Search activity title..."
           value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
           onChange={(event) =>
             table.getColumn('title')?.setFilterValue(event.target.value)
